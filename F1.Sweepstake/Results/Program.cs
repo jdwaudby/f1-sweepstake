573fda8081b39ca90be91070e2c0d2b5149acb45
James Waudby <james.waudby@blacklightsoftware.com>
2019-03-10 15:43:16 +0000

    Add solution and console app projects.

---
+++ F1.Sweepstake/Results/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Results
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
