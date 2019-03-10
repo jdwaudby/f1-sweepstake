993a144b54928d3a330caa9d56b40571c526013c
James Waudby <james.waudby@blacklightsoftware.com>
2019-03-10 15:45:31 +0000

    Add driver assignment code used during 2018 championship.

--- F1.Sweepstake/Drivers/Program.cs
+++ F1.Sweepstake/Drivers/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Drivers
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter number of participants:");
+            string input = Console.ReadLine();
+
+            int total = int.Parse(input);
+
+            string[] drivers = {
+                "Sebastian Vettel, Ferrari",
+                "Kimi Räikkönen, Ferrari",
+                "Sergio Pérez, Force India-Mercedes",
+                "Esteban Ocon, Force India-Mercedes",
+                "Romain Grosjean, Haas-Ferrari",
+                "Kevin Magnussen, Haas-Ferrari",
+                "Stoffel Vandoorne, McLaren-Renault",
+                "Fernando Alonso, McLaren-Renault",
+                "Lewis Hamilton, Mercedes",
+                "Valtteri Bottas, Mercedes",
+                "Daniel Ricciardo, Red Bull Racing-TAG Heuer",
+                "Max Verstappen, Red Bull Racing-TAG Heuer",
+                "Nico Hülkenberg, Renault",
+                "Carlos Sainz Jr., Renault",
+                "Marcus Ericsson, Sauber-Ferrari",
+                "Charles Leclerc, Sauber-Ferrari",
+                "Pierre Gasly, Scuderia Toro Rosso-Honda",
+                "Brendon Hartley, Scuderia Toro Rosso-Honda",
+                "Lance Stroll, Williams-Mercedes",
+                "Sergey Sirotkin, Williams-Mercedes"
+            };
+
+            var random = new Random();
+            int listsRequired = (total - 1) / drivers.Length + 1;
+
+            var driverList = new List<string>();
+            for (int i = 0; i < listsRequired; i++)
+            {
+                driverList.AddRange(drivers.OrderBy(x => random.Next()));
+            }
+
+            // Final list
+            foreach (var item in driverList.Take(total).Select((driver, i) => new { index = i + 1, driver }))
+            {
+                Console.WriteLine($"{item.index} {item.driver}");
+            }
         }
     }
 }
