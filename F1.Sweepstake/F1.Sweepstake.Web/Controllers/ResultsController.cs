91a75a7c8950ecd616d01bb2a6fda87346664c5d
James Waudby <james.waudby@blacklightsoftware.com>
2019-03-18 20:05:47 +0000

    Implement methods in ResultsController.
    Added Get methods.

--- F1.Sweepstake/F1.Sweepstake.Web/Controllers/ResultsController.cs
+++ F1.Sweepstake/F1.Sweepstake.Web/Controllers/ResultsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using F1.Sweepstake.Domain.Models;
+using F1.Sweepstake.Domain.Models.Ergast;
 using F1.Sweepstake.Domain.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,18 +20,36 @@ namespace F1.Sweepstake.Web.Controllers
             _resultService = resultService;
         }
 
+        // GET api/results
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Result>>> Get()
+        {
+            var results = await _resultService.Get();
+            return results.ToList();
+        }
+
+        // GET api/results
+        [HttpGet("round/{round}")]
+        public async Task<ActionResult<IEnumerable<Result>>> Get(int round)
+        {
+            var results = await _resultService.Get(round);
+            return results.ToList();
+        }
+
         // POST api/results
         [HttpPost]
         public async Task<ActionResult<IEnumerable<Player>>> Post([FromBody]IEnumerable<Player> players)
         {
-            throw new NotImplementedException();
+            players = await _resultService.Get(players);
+            return players.ToList();
         }
 
         // POST api/results/round/5
         [HttpPost("round/{round}")]
         public async Task<ActionResult<IEnumerable<Player>>> Post(int round, [FromBody]IEnumerable<Player> players)
         {
-            throw new NotImplementedException();
+            players = await _resultService.Get(round, players);
+            return players.ToList();
         }
     }
 }
