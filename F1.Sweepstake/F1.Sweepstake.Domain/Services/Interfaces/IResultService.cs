c86aeb5a402a0633a295e2a97cb79a33f099f447
James Waudby <james.waudby@blacklightsoftware.com>
2019-03-18 20:04:38 +0000

    Added Get methods to IResultService interface.

--- F1.Sweepstake/F1.Sweepstake.Domain/Services/Interfaces/IResultService.cs
+++ F1.Sweepstake/F1.Sweepstake.Domain/Services/Interfaces/IResultService.cs
@@ -1,6 +1,15 @@
-﻿namespace F1.Sweepstake.Domain.Services.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using F1.Sweepstake.Domain.Models;
+using F1.Sweepstake.Domain.Models.Ergast;
+
+namespace F1.Sweepstake.Domain.Services.Interfaces
 {
     public interface IResultService
     {
+        Task<IEnumerable<Result>> Get();
+        Task<IEnumerable<Result>> Get(int round);
+        Task<IEnumerable<Player>> Get(IEnumerable<Player> players);
+        Task<IEnumerable<Player>> Get(int round, IEnumerable<Player> players);
     }
 }
--- F1.Sweepstake/F1.Sweepstake.Domain/Services/ResultService.cs
+++ F1.Sweepstake/F1.Sweepstake.Domain/Services/ResultService.cs
@@ -1,8 +1,31 @@
-﻿using F1.Sweepstake.Domain.Services.Interfaces;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using F1.Sweepstake.Domain.Models;
+using F1.Sweepstake.Domain.Models.Ergast;
+using F1.Sweepstake.Domain.Services.Interfaces;
 
 namespace F1.Sweepstake.Domain.Services
 {
     public class ResultService : IResultService
     {
+        public async Task<IEnumerable<Result>> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Result>> Get(int round)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Player>> Get(IEnumerable<Player> players)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Player>> Get(int round, IEnumerable<Player> players)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
