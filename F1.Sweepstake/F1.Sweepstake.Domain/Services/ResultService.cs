451a877710e031cfcee80dd7fd55235b2293609c
James Waudby <james.waudby@blacklightsoftware.com>
2019-03-18 20:35:34 +0000

    Implemented non-Player ResultService.Get methods.

--- F1.Sweepstake/F1.Sweepstake.Domain/Services/ResultService.cs
+++ F1.Sweepstake/F1.Sweepstake.Domain/Services/ResultService.cs
@@ -1,21 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using F1.Sweepstake.Domain.Models;
 using F1.Sweepstake.Domain.Models.Ergast;
 using F1.Sweepstake.Domain.Services.Interfaces;
+using Newtonsoft.Json;
 
 namespace F1.Sweepstake.Domain.Services
 {
     public class ResultService : IResultService
     {
+        private static readonly HttpClient Client = new HttpClient { BaseAddress = new Uri("https://ergast.com/api/f1/") };
+
         public async Task<IEnumerable<Result>> Get()
         {
-            throw new System.NotImplementedException();
+            return await Get("last");
         }
 
         public async Task<IEnumerable<Result>> Get(int round)
         {
-            throw new System.NotImplementedException();
+            return await Get(round.ToString());
         }
 
         public async Task<IEnumerable<Player>> Get(IEnumerable<Player> players)
@@ -26,6 +32,12 @@ namespace F1.Sweepstake.Domain.Services
         public async Task<IEnumerable<Player>> Get(int round, IEnumerable<Player> players)
         {
             throw new System.NotImplementedException();
+        }
+
+        private static async Task<IEnumerable<Result>> Get(string round)
+        {
+            string resultsJson = await Client.GetStringAsync($"current/{round}/results.json");
+            return JsonConvert.DeserializeObject<Models.Ergast.RootObject>(resultsJson).MRData.RaceTable.Races.SingleOrDefault()?.Results;
         }
     }
 }
