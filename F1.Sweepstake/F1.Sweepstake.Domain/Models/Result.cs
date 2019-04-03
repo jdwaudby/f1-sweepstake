5116f72200b95e9c7286f91fe173f09141d69299
James Waudby <james.waudby@blacklightsoftware.com>
2019-04-03 12:00:30 +0100

    Stopped using Assignment as base class to Result.

--- F1.Sweepstake/F1.Sweepstake.Domain/Models/Result.cs
+++ F1.Sweepstake/F1.Sweepstake.Domain/Models/Result.cs
@@ -1,9 +1,12 @@
 ﻿namespace F1.Sweepstake.Domain.Models
 {
-    public class Result : Assignment
+    public class Result
     {
-        public bool Finished { get; set; }
         public int Position { get; set; }
+        public Player Player { get; set; }
+        public Driver Driver { get; set; }
+        public Constructor Constructor { get; set; }
+        public bool Finished { get; set; }
         public bool FastestLap { get; set; }
         public int Points { get; set; }
     }
